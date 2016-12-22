22db1e9a53158990ed69d5ae5bd26fa5b68d9e11
Kendrick Arnett <kmarnett@gmail.com>
2016-12-22 10:37:26 -0600

    Add brew update to Homebrew install.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -17,6 +17,7 @@ else
   e_success "Homebrew already installed"
 fi
 
+exec_with_status "Updating Homebrew" brew update
 exec_with_status "Tapping homebrew/bundle" brew tap homebrew/bundle
 exec_with_status "Installing from Brewfile (this could take a while)" brew bundle
 exec_with_status "Cleaning up Homebrew packages" brew cleanup

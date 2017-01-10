ed0205da358ec2c2eb36457cd683a5e67fe94fa6
Kendrick Arnett <kmarnett@gmail.com>
2017-01-10 13:37:48 -0600

    Multiplatform friendly npm installs.

--- javascript/install.sh
+++ javascript/install.sh
@@ -1,8 +1,16 @@
 #!/usr/local/bin/zsh
 
-PACKAGES=$(< ./npm-global-packages.txt)
+PACKAGES=$(tr '\n' ' ' < ./npm-global-packages.txt)
 
-exec_with_status "Installing global npm packages" npm install -g $PACKAGES --quiet
+exec_with_status "Installing global npm packages" echo $PACKAGES | xargs npm install -g --quiet
+
+if test "$(uname)" = "Darwin"
+then
+  MACOS_PACKAGES=$(tr '\n' ' ' < ./npm-global-packages.macos.txt)
+  exec_with_status "Installing macOS-specific npm packages" echo $MACOS_PACKAGES | xargs npm install -g --quiet
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+fi
 
 # Ensure the latest stable version of Node is installed
 if type "n" > /dev/null; then

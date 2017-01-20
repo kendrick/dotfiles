2cbe8fc14cec4d2bbb40248cc5dd7356d42d245d
Kendrick Arnett <kmarnett@gmail.com>
2017-01-19 20:57:57 -0600

    Add Atom config.

---
+++ atom/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ATOM_DIR=$HOME/.atom
+
+# Create ~/.atom if it doesn't exist
+if [[ ! -d "$ATOM_DIR" ]]; then
+  mkdir $ATOM_DIR
+fi
+
+for file in $DOTFILES/atom/*
+do
+  if [[ "${file: -10}" != "install.sh" ]]; then
+    ln -s $file $ATOM_DIR/$(basename $file)
+  fi
+done

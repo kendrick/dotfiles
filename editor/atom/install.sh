df53b3cd834b2bd23943a76e31f4da5399ee87a0
Kendrick Arnett <kmarnett@gmail.com>
2017-01-24 12:41:05 -0600

    Add .editorconfig & create editor directory.

--- editor/atom/install.sh
+++ editor/atom/install.sh
@@ -7,11 +7,11 @@ if [[ ! -d "$ATOM_DIR" ]]; then
   mkdir $ATOM_DIR
 fi
 
-for file in $DOTFILES/atom/*
+for file in $DOTFILES/editor/atom/*
 do
   if [[ "${file: -10}" != "install.sh" ]]; then
     ln -s $file $ATOM_DIR/$(basename $file)
   fi
 done
 
-apm install $(cat $DOTFILES/atom/packages.list)
+apm install $(cat $DOTFILES/editor/atom/packages.list)

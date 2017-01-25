695dd016b8eca1cd204ff0d7a030e261d2945cd0
Kendrick Arnett <kmarnett@gmail.com>
2017-01-25 12:48:35 -0600

    Add MAS packages to Brewfile.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,7 +10,7 @@ then
       ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
     # TODO: Create 2 Brewfiles - 1 for macOS, 1 for Linux
-    ln -s $DOTFILES_DIR/Brewfile $HOME/Brewfile
+    ln -s $DOTFILES/homebrew/Brewfile $HOME/Brewfile
 
     exec_with_status "Updating Homebrew" brew update
     exec_with_status "Tapping homebrew/bundle" brew tap homebrew/bundle

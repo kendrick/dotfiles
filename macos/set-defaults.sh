8ecdb35655834eced1090a6cf5e769744e120189
Kendrick Arnett <kmarnett@gmail.com>
2017-04-07 15:05:05 -0500

    Update trackpad behavior.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -93,8 +93,15 @@ defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool true
 # Empty Trash securely by default
 defaults write com.apple.finder EmptyTrashSecurely -bool true
 
-# Enable tap to click (trackpad)
+# Enable tap to click. (Don't have to press down on the trackpad -- just tap it.)
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+# Enable "tap-and-a-half" to drag.
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Dragging -int 1
+defaults write com.apple.AppleMultitouchTrackpad Dragging -int 1
+
+# Enable tap to click for the login screen. (May or may not work in Mavericks.)
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 

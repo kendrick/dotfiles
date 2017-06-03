9727af4ee238f26cdb3215f79e82e3b0c6c434f4
Kendrick Arnett <kmarnett@gmail.com>
2017-06-02 20:54:54 -0500

    Update broken alias.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,7 +11,7 @@ alias mkdir='mkdir -pv'
 alias o='open .'
 alias oops='TF_CMD=$(TF_ALIAS=oops PYTHONIOENCODING=utf-8 TF_SHELL_ALIASES=$(alias) thefuck $(fc -ln -1 | tail -n 1)) && eval $TF_CMD && print -s $TF_CMD'
 alias rmdsstore="find . -name '*.DS_Store' -type f -delete"
-alias sshh="ssh $SSH_HOME"
+alias sshh='ssh $SSH_HOME'
 alias ssid='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '"'"'/ SSID/ {print substr($0, index($0, $2))}'"'"''
 alias top='htop'
 alias upgrade-sketchtool='/Applications/Sketch.app/Contents/Resources/sketchtool/install.sh'

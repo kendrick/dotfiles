61b050c141df3e94c7377993e5a48d9588d8f315
Kendrick Arnett <kmarnett@gmail.com>
2020-11-09 14:09:29 -0600

    Update Homebrew packages/config.

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -2,3 +2,19 @@ alias brews='brew list -1'
 alias bubo='brew update && brew outdated'
 alias bubc='brew upgrade && brew cleanup -s'
 alias bubu='bubo && bubc'
+
+if command -v brew >/dev/null 2>&1; then
+	brew() {
+		case "$1" in
+		cleanup)
+			brew-cleanup
+			;;
+		bump)
+			brew-bump
+			;;
+		*)
+			command brew "$@"
+			;;
+		esac
+	}
+fi

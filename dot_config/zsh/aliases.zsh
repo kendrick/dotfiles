67f5138279513243f18ce0e4039b3a3a557eae14
Kendrick Arnett <kmarnett@gmail.com>
2026-09-22 16:00:08 -0500

    [auto-sync from D6VG4PXVGK] 2026-09-22 - 2 files

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -11,7 +11,6 @@ alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
 alias cls='clear && fortune | cowsay | lolcat'
-alias docker='podman'
 alias fake='faker-cli'
 alias fhere='find . -name'
 alias h='history'
